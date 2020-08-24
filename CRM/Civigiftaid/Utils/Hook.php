ae18e9debc5a73e9de94856167a2b2a1296bef67
Matthew Wire <mjw@mjwconsult.co.uk>
2020-08-24 14:07:05 +0100

    Update hook definition

--- CRM/Civigiftaid/Utils/Hook.php
+++ CRM/Civigiftaid/Utils/Hook.php
@@ -16,39 +16,46 @@ abstract class CRM_Civigiftaid_Utils_Hook extends CRM_Utils_Hook {
 
   /**
    * This hook allows filtering contributions for gift-aid
-   * @param bool    $isEligible eligibilty already detected if getDeclaration() method.
-   * @param integer $contactID  contact being checked
-   * @param date    $date  date gift-aid declaration was made on
-   * @param $contributionID  contribution id if any being referred
+   *
+   * @param bool $isEligible
+   *   eligibilty already detected if getDeclaration() method.
+   * @param int $contactID
+   *   contact being checked
+   * @param date $date
+   *   date gift-aid declaration was made on
+   * @param int $contributionID
+   *   contribution id if any being referred
    *
    * @return mixed
    */
   public static function giftAidEligible(&$isEligible, $contactID, $date = NULL, $contributionID = NULL) {
-    return self::singleton()->invoke(4, $isEligible, $contactID, $date, $contributionID, self::$_nullObject, self::$_nullObject, 'civicrm_giftAidEligible');
+    return self::singleton()->invoke(['isEligible', 'contactID', 'date', 'contributionID'], $isEligible, $contactID, $date, $contributionID, self::$_nullObject, self::$_nullObject, 'civicrm_giftAidEligible');
   }
 
   /**
    * This hook allows doing any extra processing for contributions that are added to a batch.
    *
-   * @param       $batchID
-   * @param array $contributionsAdded Contribution ids that have been batched
+   * @param int $batchID
+   * @param array $contributionsAdded
+   *   Contribution ids that have been batched
    *
    * @return mixed
    */
-  public static function batchContributions( $batchID, $contributionsAdded ) {
-    return self::singleton()->invoke(2, $batchID, $contributionsAdded, self::$_nullObject, self::$_nullObject, self::$_nullObject, self::$_nullObject, 'civicrm_batchContributions');
+  public static function batchContributions($batchID, $contributionsAdded) {
+    return self::singleton()->invoke(['batchID', 'contributionsAdded'], $batchID, $contributionsAdded, self::$_nullObject, self::$_nullObject, self::$_nullObject, self::$_nullObject, 'civicrm_batchContributions');
   }
 
   /**
    * This hook allows altering getDeclaration() query
-   * @param string $query  declaration query
-   * @param array  $queryParams  params required by query
+   * @param string $query
+   *   declaration query
+   * @param array $queryParams
+   *   params required by query
    *
    * @return mixed
-   *
    */
-  public static function alterDeclarationQuery( &$query, &$queryParams ) {
-    return self::singleton()->invoke(2, $query, $queryParams, self::$_nullObject, self::$_nullObject, self::$_nullObject, self::$_nullObject, 'civicrm_alterDeclarationQuery');
+  public static function alterDeclarationQuery(&$query, &$queryParams) {
+    return self::singleton()->invoke(['query', 'queryParams'], $query, $queryParams, self::$_nullObject, self::$_nullObject, self::$_nullObject, self::$_nullObject, 'civicrm_alterDeclarationQuery');
   }
 
 }
